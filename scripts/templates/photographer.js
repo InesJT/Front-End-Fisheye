7c5059457570a97afa7dbfaf688e161f78b3961c
Ines Trabelsi <ines.jenfaoui@gmail.com>
2024-11-10 17:49:00 +0100

    feat: fetch photografers data and create card DOM

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,31 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { id, name, city, country, price, tagline, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const link = document.createElement('a');
+        link.setAttribute('href', `./photographer.html?${id}`);
+        link.setAttribute('class', 'linkCard');
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        const h2 = document.createElement('h2');
+        h2.innerText = name;
+        const h3 = document.createElement('h3');
+        h3.textContent = city + ", " + country;
+        const h4 = document.createElement('h4');
+        h4.textContent = tagline;
+        const p = document.createElement('p');
+        p.textContent = price + " €/jour";
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(p);
+        link.append(article);
+        return link;
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM };
 }
