6f373ae30219aa87c0311b64a0a26c5ad6c781cf
Ines Trabelsi <ines.jenfaoui@gmail.com>
2024-11-26 23:19:51 +0100

    feat: Add sort media with 3 criterias

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -28,6 +28,57 @@ function updateTotalLikes(medias) {
   likesSumWidget.innerText = likesSum;
 }
 
+function sortMedia(medias, photographer) {
+  const criteria = document.getElementById("criteria");
+  criteria.addEventListener("change", () => {
+    // trie par popularité
+    if (criteria.value === "0") {
+      medias.sort((a, b) => b.likes - a.likes);
+      // trie par date
+    } else if (criteria.value === "1") {
+      medias.sort((a, b) => {
+        if (a.date < b.date) return -1;
+        if (a.date > b.date) return 1;
+        return 0;
+      });
+      // trie par titre
+    } else if (criteria.value === "2") {
+      medias.sort((a, b) => {
+        if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;
+        if (a.title.toLowerCase() > b.title.toLowerCase()) return 1;
+        return 0;
+      });
+    } else {
+      console.log("Critère de tri inconnu");
+    }
+
+    createMediaSection(medias, photographer);
+
+    // Initialiser le lightbox après avoir charger les images
+    new LightBoxTemplate().create();
+
+    handleLikeButtons(medias);
+    // MAJ total des like
+    updateTotalLikes(medias);
+  });
+}
+
+function createMediaSection(medias, photographer) {
+  const mediaSection = document.querySelector(".media");
+  mediaSection.innerHTML = "";
+  let totalLikes = 0;
+  medias.forEach((media) => {
+    let card;
+    if (media.image) {
+      card = new MediaFactory({ ...media, photographerName: photographer.name }, "image");
+    } else {
+      card = new MediaFactory({ ...media, photographerName: photographer.name }, "video");
+    }
+    mediaSection.append(card)
+    totalLikes = totalLikes + media.likes;
+  });
+}
+
 async function init() {
   const params = new URL(document.location).searchParams;
   const id = parseInt(params.get("id")); // Récupérer la valeur de l'id à partir de l'URL
@@ -42,27 +93,13 @@ async function init() {
 
   new PhotographerFactory(photographer, "detail");
 
-  const mediaSection = document.querySelector(".media");
-  mediaSection.innerHTML = "";
-  let totalLikes = 0;
-  medias.forEach((media) => {
-    let card;
-    if (media.image) {
-      card = new MediaFactory({ ...media, photographerName: photographer.name }, "image");
-    } else {
-      card = new MediaFactory({ ...media, photographerName: photographer.name }, "video");
-    }
-    mediaSection.append(card)
-    totalLikes = totalLikes + media.likes;
-    console.log(media.likes);
-  });
-  console.log(totalLikes);
+  createMediaSection(medias, photographer);
 
   new LightBoxTemplate().create();
 
   handleLikeButtons(medias);
   updateTotalLikes(medias);
-
+  sortMedia(medias, photographer);
 }
 
 init();
